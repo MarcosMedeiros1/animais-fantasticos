c954003e338f073d4d31c0b203a55faa0b4590af
Marcos <mmarcos787@gmail.com>
2022-04-12 16:05:39 -0300

    Criação do Dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import initSmoothScroll from "./modules/smooth-scroll.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 import initAnimeScroll from "./modules/anime-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTooltip from "./modules/tooltip.js";
@@ -6,6 +7,7 @@ import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 
 initSmoothScroll();
+initDropdownMenu();
 initAnimeScroll();
 initAccordion();
 initTooltip();
