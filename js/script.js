ef661821c48814a25e2ea352b81289b8076bcf89
Marcos <mmarcos787@gmail.com>
2022-04-11 21:28:33 -0300

    Criaçõ de Tooltip

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 import initSmoothScroll from "./modules/smooth-scroll.js";
 import initAnimeScroll from "./modules/anime-scroll.js";
 import initAccordion from "./modules/accordion.js";
+import initTooltip from "./modules/tooltip.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 
 initSmoothScroll();
 initAnimeScroll();
 initAccordion();
+initTooltip();
 initTabNav();
 initModal();
