1a44fe49435816ce471ff226c1f960221daec167
Marcos Medeiros <mmarcos787@gmail.com>
2023-02-25 18:46:50 -0300

    refatorando accordion

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 import initFuncioanmento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
-import SmoothScroll from "./modules/smooth-scroll.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAccordion from "./modules/accordion.js";
+import SmoothScroll from "./modules/smooth-scroll.js";
 import initTooltip from "./modules/tooltip.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 
-initFuncioanmento();
-initFetchAnimais();
 const smoothScroll = new SmoothScroll("[data-menu='suave'] a[href^='#']");
 smoothScroll.init();
+
+const accordion = new Accordion("[data-anime='accordion'] dt");
+accordion.init();
+
+initFuncioanmento();
+initFetchAnimais();
 initDropdownMenu();
 initFetchBitcoin();
 initAnimaScroll();
 initMenuMobile();
-initAccordion();
 initTooltip();
 initTabNav();
 initModal();
