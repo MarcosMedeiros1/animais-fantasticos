7b9cfad9a2073a800b8d34e706426d1334d38e5e
Marcos Medeiros <mmarcos787@gmail.com>
2023-03-05 17:50:35 -0300

    anima-scroll refeito

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -3,7 +3,27 @@ export default class AnimaScroll {
     this.sections = document.querySelectorAll(sections);
     this.windowHalf = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
+  }
+
+  getSectionsDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowHalf),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("active");
+      } else if (item.element.classList.contains(".active")) {
+        item.element.classList.remove(".active");
+      }
+    });
   }
 
   animaScroll() {
@@ -20,8 +40,15 @@ export default class AnimaScroll {
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener("scroll", this.animaScroll);
+    if (this.sections.length) {
+      this.getSectionsDistance();
+      this.animaScroll();
+      window.addEventListener("scroll", this.checkDistance);
+    }
     return this;
+  }
+
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
